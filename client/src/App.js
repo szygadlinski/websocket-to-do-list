756955dbf7ad6a73cc77ea7e4d7cd626fc317654
Szymon Zygadliński <szygadlinski@gmail.com>
2021-04-26 01:43:46 +0200

    Add client code and styles

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,31 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        Hello world!
+
+        <header>
+          <h1>ToDoList.app</h1>
+        </header>
+
+        <section className="tasks-section" id="tasks-section">
+          <h2>Tasks</h2>
+
+          <ul className="tasks-list" id="tasks-list">
+            <li className="task">Shopping <button className="btn btn-red">Remove</button></li>
+            <li className="task">Go out with a dog <button className="btn btn-red">Remove</button></li>
+          </ul>
+
+          <form id="add-task-form">
+            <input
+              className="text-input"
+              id="text-input"
+              type="text"
+              placeholder="Type your description"
+              autoComplete="off"
+            />
+            <button className="btn" type="submit">Add</button>
+          </form>
+
+        </section>
       </div>
     )
   }
