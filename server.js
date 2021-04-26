c80fa63888e293de9373fe202a2d3c201d580d75
Szymon Zygadliński <szygadlinski@gmail.com>
2021-04-26 02:07:24 +0200

    Add socket.io and add listeners to server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 
+const tasks = [];
 
 app.use((req, res) => {
   res.status(404).send('<h1>404 - page not found...</h1>');
@@ -9,4 +11,20 @@ app.use((req, res) => {
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port 8000: http://localhost:8000');
+});
+
+const io = socket(server);
+
+io.on('connection', socket => {
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', task => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', tasks);
+  });
+
+  socket.on('removeTask', taskId => {
+    tasks.splice(taskId, 1);
+    socket.broadcast.emit('removeTask', tasks);
+  });
 });
