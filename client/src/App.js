db2026a4182300d7e01980d1a3f06821de01d20a
Szymon Zygadliński <szygadlinski@gmail.com>
2021-04-27 02:12:17 +0200

    Add socket.io and functionality to client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,59 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 class App extends React.Component {
+  state = {
+    tasks: [],
+    taskName: '',
+  }
+
+  componentDidMount() {
+    this.socket = io.connect('http://localhost:8000', {
+      transports: ['websocket'],
+    });
+
+    this.socket.on('addTask', event => this.addTask(event));
+    this.socket.on('removeTask', event => this.removeTask(event));
+    this.socket.on('updateData', event => this.updateTasks(event));
+  }
+
+  updateTasks = newTasks => {
+    this.setState({ tasks: newTasks });
+  }
+
+  addTask = task => {
+    let newTasks = this.state.tasks;
+    if (!task || !task.trim()) {
+      alert('You have to type something first!');
+    } else if (newTasks.includes(task)) {
+      alert('Your task is already on the list!');
+    } else {
+      newTasks.push(task);
+      this.setState({ tasks: newTasks })
+    }
+  }
+
+  removeTask = (index, localRemoval) => {
+    let newTasks = [];
+    for(let task of this.state.tasks){
+      if(this.state.tasks.indexOf(task) !== index) newTasks.push(task)
+    }
+    this.setState({ tasks: newTasks });
+    if(localRemoval) this.socket.emit('removeTask', index)
+  }
+
+  submitForm = event => {
+    event.preventDefault();
+    this.addTask(this.state.taskName);
+    this.socket.emit('addTask', this.state.taskName)
+    this.setState({ taskName: '' });
+  }
 
   render() {
+    const { tasks, taskName } = this.state;
+
     return (
       <div className="App">
-
         <header>
           <h1>ToDoList.app</h1>
         </header>
@@ -14,17 +62,28 @@ class App extends React.Component {
           <h2>Tasks</h2>
 
           <ul className="tasks-list" id="tasks-list">
-            <li className="task">Shopping <button className="btn btn-red">Remove</button></li>
-            <li className="task">Go out with a dog <button className="btn btn-red">Remove</button></li>
+            {tasks.map(task => (
+              <li className="task" key={tasks.indexOf(task)}>
+                {task}
+                <button
+                  className="btn btn-red"
+                  onClick={localRemoval => this.removeTask(tasks.indexOf(task), localRemoval)}
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
           </ul>
 
-          <form id="add-task-form">
+          <form id="add-task-form" onSubmit={event => this.submitForm(event)}>
             <input
               className="text-input"
               id="text-input"
               type="text"
               placeholder="Type your description"
               autoComplete="off"
+              value={taskName}
+              onChange={event => this.setState({ taskName: event.target.value })}
             />
             <button className="btn" type="submit">Add</button>
           </form>
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const socket = require('socket.io');
 
 const app = express();
 
 const tasks = [];
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use((req, res) => {
   res.status(404).send('<h1>404 - page not found...</h1>');
@@ -18,13 +26,13 @@ const io = socket(server);
 io.on('connection', socket => {
   socket.emit('updateData', tasks);
 
-  socket.on('addTask', task => {
-    tasks.push(task);
-    socket.broadcast.emit('addTask', tasks);
+  socket.on('addTask', taskName => {
+    tasks.push(taskName);
+    socket.broadcast.emit('addTask', taskName);
   });
 
-  socket.on('removeTask', taskId => {
-    tasks.splice(taskId, 1);
-    socket.broadcast.emit('removeTask', tasks);
+  socket.on('removeTask', taskIndex => {
+    tasks.splice(taskIndex, 1);
+    socket.broadcast.emit('removeTask', taskIndex);
   });
 });
